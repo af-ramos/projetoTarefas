149471ed7654984fb5a86dbce13b9ad89b55fa80
Arthur Ramos <arfrra@hotmail.com>
2025-05-17 12:49:24 -0300

    Removendo notification

--- app/Services/LogService.php
+++ app/Services/LogService.php
@@ -28,11 +28,4 @@ class LogService
             'error' => $error
         ]);
     }
-
-    public function notification(int $user, string $type, array $body) {
-        return $this->mongoRepository->notification([
-            'user' => $user, 'type' => $type, 'body' => $body,
-            'date' => now()->format('Y-m-d'), 'time' => now()->format('H:i:s')
-        ]);
-    }
 }
