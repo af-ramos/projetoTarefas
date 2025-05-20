610a492eb4663b7497d620c34322690118863274
Arthur Ramos <arfrra@hotmail.com>
2025-05-20 19:12:29 -0300

    Ajuste

--- app/Services/Notification/NotificationService.php
+++ app/Services/Notification/NotificationService.php
@@ -2,42 +2,31 @@
 
 namespace App\Services\Notification;
 
-use App\Models\Notification;
 use App\Services\UserService;
-use Illuminate\Database\Eloquent\Collection;
 
 abstract class NotificationService
 {
     protected UserService $userService;
-
-    protected Collection $userNotifications;
-    protected array $content;
 
     public function __construct(UserService $userService) {
         $this->userService = $userService;
     }
     
     public function init(array $data) {
-        $this->setUserNotifications($data['target']);
-        $this->setContent($data);
-
-        $this->sendMessage();
+        $userNotifications = $this->setUserNotifications($data['target']);
+        $this->sendMessage($userNotifications, $data);
     }
 
     public function setUserNotifications(int $user) {
-        $this->userNotifications = $this->userService->getUserNotifications($user);
+        return $this->userService->getUserNotifications($user);
     }
 
-    public function setContent(array $data) {
-        $this->content = $data;
-    }
-
-    public function sendMessage() {
-        foreach ($this->userNotifications as $notificationType) {
+    public function sendMessage(mixed $userNotifications, array $data) {
+        foreach ($userNotifications as $notificationType) {
             $sendMethod = 'send' . ucfirst(strtolower($notificationType->description));
 
             if (method_exists($this, $sendMethod)) {
-                $this->$sendMethod($this->content);
+                $this->$sendMethod($data);
             }
         }
     }
