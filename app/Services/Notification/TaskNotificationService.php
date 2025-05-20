a180c9972d0c3501122a478839e42af23fb6681a
Arthur Ramos <arfrra@hotmail.com>
2025-05-20 20:53:52 -0300

    Finalizando projeto

--- app/Services/Notification/TaskNotificationService.php
+++ app/Services/Notification/TaskNotificationService.php
@@ -15,11 +15,17 @@ class TaskNotificationService extends NotificationService
     }
 
     private function sendTaskCreatedEmail(array $data) {
+        // definir o body do email para task criada
+        // realizar envio do email para task criada
+
         Log::info('sendTaskCreatedEmail');
         Log::info($data);
     }
 
     private function sendTaskAssignedEmail(array $data) {
+        // definir o body do email para task atribuida
+        // realizar o envio do email para task atribuida
+
         Log::info('sendTaskAssignedEmail');
         Log::info($data);
     }
